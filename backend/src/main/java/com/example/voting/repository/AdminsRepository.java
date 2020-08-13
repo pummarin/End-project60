ef3bbf62bb9bcdeccc27e9716d55079527a4c697
pummarin <pummarinb6003296@gmail.com>
2020-08-13 22:14:04 +0700

    add adminsrepository - close #16

--- backend/src/main/java/com/example/voting/repository/AdminsRepository.java
+++ backend/src/main/java/com/example/voting/repository/AdminsRepository.java
@@ -1,11 +1,12 @@
 package com.example.voting.repository;
 
-import com.example.voting.entity.Vote;
-
+import com.example.voting.entity.Admins;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-	Vote findById(long id);
+public interface AdminsRepository extends JpaRepository<Admins, Long> {
+    Optional<Admins> findById(Long id);
 }
